817dc784f2d790be9df549d1dd6769f0f2b6b39e
Elon Musk <elon@mu.sk>
2024-06-06 08:37:41 +0200

    Change the app name

--- src/test/kotlin/no/nb/mlt/wls/HermnesApplicationTests.kt
+++ src/test/kotlin/no/nb/mlt/wls/HermnesApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class BifrostApplicationTests {
+class HermnesApplicationTests {
 
 	@Test
 	fun contextLoads() {
