45d3af8ba7722b61192937634704d5e19f9e98dd
Daniel Aaron Salwerowicz <daniel.salwerowicz@nb.no>
2024-06-13 11:29:02 +0200

    Test Qodana functionality

--- src/main/kotlin/no/nb/mlt/wls/HermesApplication.kt
+++ src/main/kotlin/no/nb/mlt/wls/HermesApplication.kt
@@ -1,5 +1,6 @@
 package no.nb.mlt.wls
 
+import org.reactivestreams.Publisher
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,4 +9,12 @@ class HermesApplication
 
 fun main(args: Array<String>) {
     runApplication<HermesApplication>(*args)
+}
+
+// Test Qodana, delete later
+class Dummy : Publisher<String> {
+    override fun subscribe(s: org.reactivestreams.Subscriber<in String>?) {
+        val i = if (s != null) 1 else 1
+        throw UnsupportedOperationException("not implemented")
+    }
 }
