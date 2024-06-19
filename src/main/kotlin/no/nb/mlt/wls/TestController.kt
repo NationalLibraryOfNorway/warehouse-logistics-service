447c6e2629b806117e56ee43a2f73fd2a0e3ea97
Daniel Aaron Salwerowicz <daniel.salwerowicz@nb.no>
2024-06-19 15:26:58 +0200

    MLT-0014 Create a secured endpoint with Keycloak (#5)

---
+++ src/main/kotlin/no/nb/mlt/wls/TestController.kt
@@ -0,0 +1,36 @@
+package no.nb.mlt.wls
+
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.Authentication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/test")
+class TestController {
+    @GetMapping("/open", produces = [APPLICATION_JSON_VALUE])
+    fun open(): ResponseEntity<Response> {
+        return ResponseEntity.ok(Response("Hello to an open endpoint!"))
+    }
+
+    @GetMapping("/authenticated", produces = [APPLICATION_JSON_VALUE])
+    fun authenticated(authentication: Authentication): ResponseEntity<Response> {
+        return ResponseEntity.ok(Response("Hello ${authentication.name} to an authenticated endpoint!"))
+    }
+
+    @GetMapping("/authorized", produces = [APPLICATION_JSON_VALUE])
+    fun authorized(authentication: Authentication): ResponseEntity<Response> {
+        return ResponseEntity.ok(
+            Response(
+                """
+                Hello ${authentication.name} to an endpoint that only users with the 'wls-dev-role' authority can access!
+                You have following authorities: ${authentication.authorities.joinToString(", ")}
+                """.trimIndent()
+            )
+        )
+    }
+
+    class Response(val message: String)
+}
--- src/test/kotlin/no/nb/mlt/wls/HermesApplicationTests.kt
+++ src/test/kotlin/no/nb/mlt/wls/HermesApplicationTests.kt
@@ -4,14 +4,13 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class BifrostApplicationTests {
+class HermesApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
 
-	@Test
-	fun contextLoads() {
-	}
-
-  @Test
-  fun itsFunToNOTFail() {
-    assert(true)
-  }
+    @Test
+    fun itsFunToNOTFail() {
+        assert(true)
+    }
 }
