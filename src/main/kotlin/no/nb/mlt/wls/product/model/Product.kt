25a219e0a72420653da4a4fac1bfe9d98e34b0c2
Daniel Aaron Salwerowicz <daniel.salwerowicz@nb.no>
2024-07-04 15:00:14 +0200

    Fix collection mapping for products

--- src/main/kotlin/no/nb/mlt/wls/product/model/Product.kt
+++ src/main/kotlin/no/nb/mlt/wls/product/model/Product.kt
@@ -4,7 +4,9 @@ import no.nb.mlt.wls.core.data.HostName
 import no.nb.mlt.wls.core.data.Owner
 import no.nb.mlt.wls.core.data.Packaging
 import no.nb.mlt.wls.core.data.PreferredEnvironment
+import org.springframework.data.mongodb.core.mapping.Document
 
+@Document(collection = "products")
 data class Product(
     val hostName: HostName,
     val hostId: String,
