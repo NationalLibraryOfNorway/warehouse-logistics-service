8461f5792f88710deb41402bba8c82658b8dfb79
Daniel Aaron Salwerowicz <daniel.salwerowicz@nb.no>
2024-07-10 12:51:15 +0200

    Cleaned up SecurityConfig, updated local Keycloak realm export, removed TestController
    SecurityConfig we had was thrown together quick and dirty by me, before having a proper know-how about it. Now that I know a tiny bit more I was able to simplify security configuration.
    
    Authorization is now scope based, rather than role based. I think it might fit our use case better.

--- src/main/kotlin/no/nb/mlt/wls/core/config/SecurityConfig.kt
+++ src/main/kotlin/no/nb/mlt/wls/core/config/SecurityConfig.kt
@@ -4,110 +4,43 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import org.springframework.core.convert.converter.Converter
-import org.springframework.http.HttpMethod
-import org.springframework.security.authentication.AbstractAuthenticationToken
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
-import org.springframework.security.oauth2.core.OAuth2Error
-import org.springframework.security.oauth2.core.OAuth2TokenValidator
-import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult
-import org.springframework.security.oauth2.jwt.Jwt
-import org.springframework.security.oauth2.jwt.JwtValidators
-import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
-import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter
+import org.springframework.security.config.web.server.invoke
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.JwtDecoders
 import org.springframework.security.web.server.SecurityWebFilterChain
-import reactor.core.publisher.Mono
 
 @Configuration
 @Profile("!pipeline")
 @EnableWebFluxSecurity
-@EnableMethodSecurity(prePostEnabled = true)
 class SecurityConfig {
     @Value("\${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
     val issuerUri: String = ""
 
     @Bean
     fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
-        http
-            .csrf { csrf -> csrf.disable() }
-            .authorizeExchange { exchange ->
-                exchange
-                    .pathMatchers(
-                        "/api-docs",
-                        "/api-docs/**",
-                        "/swagger",
-                        "/swagger/**",
-                        "/swagger-ui/**",
-                        "/swagger-ui.html",
-                        "/swagger-resources",
-                        "/swagger-resources/**",
-                        "/webjars/swagger-ui/**",
-                        "/actuator",
-                        "/actuator/**"
-                    ).permitAll()
-                    // TODO - Move these to the respective controllers endpoints once ready
-                    .pathMatchers(HttpMethod.GET, "/v1/test/authorized").hasAuthority("wls-role")
-                    .pathMatchers(HttpMethod.GET, "/v1/test/authenticated").authenticated()
-                    .pathMatchers("/v1/test/json").authenticated()
-                    .pathMatchers("/v1/product").authenticated()
-                    .pathMatchers(HttpMethod.GET, "/v1/test/open").permitAll()
+        return http {
+            cors {
+                disable()
             }
-            .oauth2ResourceServer { server ->
-                server.jwt { jwt ->
-                    jwt.jwtAuthenticationConverter(authoritiesExtractor())
-                    jwt.jwtDecoder(jwtDecoder())
-                }
+            authorizeExchange {
+                authorize("/api-docs", permitAll)
+                authorize("/api-docs/**", permitAll)
+                authorize("/swagger", permitAll)
+                authorize("/webjars/swagger-ui/**", permitAll)
+                authorize("/actuator", permitAll)
+                authorize("/actuator/**", permitAll)
+                authorize("/v1/product", hasAuthority("SCOPE_wls-product-scope"))
+                authorize("/v1/order", hasAuthority("SCOPE_wls-order-scope"))
+                authorize(anyExchange, authenticated)
             }
-        return http.build()
-    }
-
-    @Bean
-    fun authoritiesExtractor(): Converter<Jwt, Mono<AbstractAuthenticationToken>> {
-        val jwtAuthConverter = JwtAuthenticationConverter()
-        jwtAuthConverter.setJwtGrantedAuthoritiesConverter(GrantedAuthoritiesExtractor())
-        jwtAuthConverter.setPrincipalClaimName("preferred_username")
-        return ReactiveJwtAuthenticationConverterAdapter(jwtAuthConverter)
-    }
-
-    internal class GrantedAuthoritiesExtractor : Converter<Jwt, Collection<GrantedAuthority>> {
-        override fun convert(jwt: Jwt): Collection<GrantedAuthority> {
-            @Suppress("UNCHECKED_CAST")
-            val realmAccess = jwt.claims.getOrDefault("realm_access", emptyMap<String, List<String>>()) as Map<String, List<String>>
-            val roles = realmAccess.getOrDefault("roles", emptyList())
-            return roles.map { SimpleGrantedAuthority(it) }
+            oauth2ResourceServer {
+                jwt { }
+            }
         }
     }
 
     @Bean
-    fun jwtDecoder(): ReactiveJwtDecoder {
-        val jwtDecoder = ReactiveJwtDecoders.fromIssuerLocation(issuerUri) as NimbusReactiveJwtDecoder
-        val audienceValidator = AudienceValidator()
-        val withIssuer = JwtValidators.createDefaultWithIssuer(issuerUri)
-        val withAudience = DelegatingOAuth2TokenValidator(withIssuer, audienceValidator)
-        jwtDecoder.setJwtValidator(withAudience)
-
-        return jwtDecoder
-    }
-
-    internal class AudienceValidator : OAuth2TokenValidator<Jwt> {
-        private val error: OAuth2Error = OAuth2Error("invalid_token", "The required audience is missing", null)
-
-        override fun validate(jwt: Jwt): OAuth2TokenValidatorResult {
-            // Not sure if I completely understand how the audience is supposed to be used here.
-            // Can be changed as needed when we actually start authenticating real services and requests.
-            return if (jwt.audience.contains("wls")) {
-                OAuth2TokenValidatorResult.success()
-            } else {
-                OAuth2TokenValidatorResult.failure(error)
-            }
-        }
-    }
+    fun jwtDecoder(): JwtDecoder = JwtDecoders.fromIssuerLocation(issuerUri)
 }
