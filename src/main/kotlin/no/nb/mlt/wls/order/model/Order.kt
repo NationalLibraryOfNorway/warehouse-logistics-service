6a70f801d27e6c0366554dd8f980ee005dfd0352
Daniel Aaron Salwerowicz <daniel.salwerowicz@nb.no>
2024-07-23 15:12:40 +0200

    Added base model for order

---
+++ src/main/kotlin/no/nb/mlt/wls/order/model/Order.kt
@@ -0,0 +1,51 @@
+package no.nb.mlt.wls.order.model
+
+import no.nb.mlt.wls.core.data.HostName
+import no.nb.mlt.wls.core.data.Owner
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document(collection = "orders")
+data class Order (
+    val hostName: HostName,
+    val hostOrderId: String,
+    val status: OrderStatus,
+    val productLine: List<ProductLine>,
+    val orderType: OrderType,
+    val receiver: OrderReceiver,
+    val callbackUrl: String,
+    val owner: Owner
+)
+
+data class ProductLine (
+    val hostId: String,
+    val status: OrderStatus?
+)
+
+data class OrderReceiver (
+    val name: String,
+    val location: String,
+    val address: String?,
+    val city: String?,
+    val postalCode: String?,
+    val phoneNumber: String?
+)
+
+enum class OrderStatus(private val status: String) {
+    NOT_STARTED("Not started"),
+    IN_PROGRESS("In progress"),
+    COMPLETED("Completed"),
+    DELETED("Deleted");
+
+    override fun toString(): String {
+        return status
+    }
+}
+
+enum class OrderType(private val type: String) {
+    LOAN("Loan"),
+    DIGITIZATION("Digitization"),;
+
+    override fun toString(): String {
+        return type
+    }
+}
