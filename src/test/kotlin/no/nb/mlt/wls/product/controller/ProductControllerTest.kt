594bbab5a72e19911e035f5e351a86bd1953d6dd
Noah Bjerkli Aanonli <noah.aanonli@nb.no>
2024-08-05 13:29:52 +0200

    Migrate to dummy-synq for integration tests

--- src/test/kotlin/no/nb/mlt/wls/product/controller/ProductControllerTest.kt
+++ src/test/kotlin/no/nb/mlt/wls/product/controller/ProductControllerTest.kt
@@ -1,6 +1,5 @@
 package no.nb.mlt.wls.product.controller
 
-import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
 import io.mockk.junit5.MockKExtension
 import no.nb.mlt.wls.EnableTestcontainers
@@ -26,14 +25,12 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDO
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.http.ResponseEntity
 import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.csrf
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.expectBody
 import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.server.ServerErrorException
-import java.net.URI
 
 @EnableTestcontainers
 @TestInstance(PER_CLASS)
@@ -42,11 +39,9 @@ import java.net.URI
 @EnableMongoRepositories("no.nb.mlt.wls")
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class ProductControllerTest(
-    @Autowired val repository: ProductRepository
+    @Autowired val repository: ProductRepository,
+    @Autowired val synqProductService: SynqProductService
 ) {
-    @MockkBean
-    private lateinit var synqProductService: SynqProductService
-
     private lateinit var webTestClient: WebTestClient
 
     @BeforeEach
@@ -64,10 +59,6 @@ class ProductControllerTest(
     @Test
     @WithMockUser
     fun `createProduct with valid payload creates product`() {
-        every {
-            synqProductService.createProduct(any())
-        } returns ResponseEntity.created(URI.create("")).build()
-
         webTestClient
             .mutateWith(csrf())
             .post()
@@ -124,10 +115,6 @@ class ProductControllerTest(
     @Test
     @WithMockUser
     fun `createProduct where SynQ says it's a duplicate returns OK`() {
-        every {
-            synqProductService.createProduct(any())
-        } returns ResponseEntity.ok().build()
-
         webTestClient
             .mutateWith(csrf())
             .post()
