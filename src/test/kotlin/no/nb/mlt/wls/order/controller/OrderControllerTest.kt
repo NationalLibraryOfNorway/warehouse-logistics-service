25c1c915f359fa1136fbd6b41a6a20a9b9d24213
Noah Bjerkli Aanonli <noah.aanonli@nb.no>
2024-08-20 12:14:56 +0200

    Happy-test

--- src/test/kotlin/no/nb/mlt/wls/order/controller/OrderControllerTest.kt
+++ src/test/kotlin/no/nb/mlt/wls/order/controller/OrderControllerTest.kt
@@ -1,6 +1,7 @@
 package no.nb.mlt.wls.order.controller
 
 import com.ninjasquad.springmockk.MockkBean
+import io.mockk.coEvery
 import io.mockk.junit5.MockKExtension
 import kotlinx.coroutines.reactor.awaitSingle
 import kotlinx.coroutines.runBlocking
@@ -18,6 +19,7 @@ import no.nb.mlt.wls.order.payloads.toOrder
 import no.nb.mlt.wls.order.repository.OrderRepository
 import no.nb.mlt.wls.order.service.OrderService
 import no.nb.mlt.wls.order.service.SynqOrderService
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -28,8 +30,12 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.csrf
 import org.springframework.test.web.reactive.server.WebTestClient
+import java.net.URI
 
 @EnableTestcontainers
 @TestInstance(PER_CLASS)
@@ -61,6 +67,25 @@ class OrderControllerTest(
     @WithMockUser
     fun `createOrder with valid payload creates order`() =
         runTest {
+            coEvery {
+                synqOrderService.createOrder(any())
+            } returns ResponseEntity.created(URI.create("")).build()
+
+            webTestClient
+                .mutateWith(csrf())
+                .post()
+                .uri("/batch/create")
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(testOrderPayload)
+                .exchange()
+                .expectStatus().isCreated
+
+            val order = repository.findByHostNameAndHostOrderId(testOrderPayload.hostName, testOrderPayload.hostOrderId).awaitSingle()
+
+            assertThat(order)
+                .isNotNull
+                .extracting("callbackUrl", "status")
+                .containsExactly("callbackUrl", "Not started")
         }
 
     @Test
