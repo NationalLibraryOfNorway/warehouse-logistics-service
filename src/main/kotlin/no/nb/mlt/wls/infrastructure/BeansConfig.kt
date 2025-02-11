12fd6064692c5596036eba47be470ddddfc90e69
Noah Bjerkli Aanonli <noah.aanonli@nb.no>
2025-02-11 11:49:35 +0100

    Allow for email server to be disabled
    This allows for full disabling the email adapter by simply not
    providing the env-variables Spring needs
    to create a JavaMailSender.

--- src/main/kotlin/no/nb/mlt/wls/infrastructure/BeansConfig.kt
+++ src/main/kotlin/no/nb/mlt/wls/infrastructure/BeansConfig.kt
@@ -1,18 +1,23 @@
 package no.nb.mlt.wls.infrastructure
 
 import no.nb.mlt.wls.domain.WLSService
+import no.nb.mlt.wls.domain.ports.outbound.EmailNotifier
 import no.nb.mlt.wls.infrastructure.callbacks.InventoryNotifierAdapter
+import no.nb.mlt.wls.infrastructure.email.DisabledEmailAdapter
 import no.nb.mlt.wls.infrastructure.email.EmailAdapter
 import no.nb.mlt.wls.infrastructure.repositories.item.ItemRepositoryMongoAdapter
 import no.nb.mlt.wls.infrastructure.repositories.mail.MongoEmailRepositoryAdapter
 import no.nb.mlt.wls.infrastructure.repositories.order.MongoOrderRepositoryAdapter
 import no.nb.mlt.wls.infrastructure.synq.SynqAdapter
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.web.reactive.result.view.freemarker.FreeMarkerConfigurer
 import org.springframework.web.reactive.result.view.freemarker.FreeMarkerViewResolver
 
+// TODO - Rename and split this file into relevant/specific configs?
 @Configuration
 class BeansConfig {
     @Bean
@@ -21,9 +26,14 @@ class BeansConfig {
         itemMongoAdapter: ItemRepositoryMongoAdapter,
         orderMongoAdapter: MongoOrderRepositoryAdapter,
         callbackHandler: InventoryNotifierAdapter,
-        emailAdapter: EmailAdapter
+        emailAdapter: EmailNotifier
     ) = WLSService(itemMongoAdapter, orderMongoAdapter, synqAdapter, callbackHandler, emailAdapter)
 
+    @ConditionalOnMissingBean(JavaMailSender::class)
+    @Bean
+    fun disabledEmailAdapter() = DisabledEmailAdapter()
+
+    @ConditionalOnBean(JavaMailSender::class)
     @Bean
     fun emailAdapter(
         emailRepository: MongoEmailRepositoryAdapter,
