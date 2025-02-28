1b5979f300485d6d8c2300fcdd9f9d7fb2e0ddca
Daniel Aaron Salwerowicz <Daniel@Salwerowi.cz>
2025-02-28 12:08:46 +0100

    Hotfix Use Proxy for External Calls (#65)
    Set up proxy support, fix style for setting headers in callbacks

--- src/main/kotlin/no/nb/mlt/wls/infrastructure/WebClientConfig.kt
+++ src/main/kotlin/no/nb/mlt/wls/infrastructure/WebClientConfig.kt
@@ -2,10 +2,18 @@ package no.nb.mlt.wls.infrastructure
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient
 
 @Configuration
 class WebClientConfig {
     @Bean
-    fun webClient(builder: WebClient.Builder): WebClient = builder.build()
+    fun webClient(builder: WebClient.Builder): WebClient {
+        val httpClient = HttpClient.create().proxyWithSystemProperties()
+
+        return builder
+            .clientConnector(ReactorClientHttpConnector(httpClient))
+            .build()
+    }
 }
