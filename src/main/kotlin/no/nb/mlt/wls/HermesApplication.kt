2704621e3fab1ecf97fd8b281940b278d91d307d
tomolse <71635055+tomolse@users.noreply.github.com>
2025-03-03 08:23:11 +0100

    Cleanup test proxy settings and HermesApplication.kt (#68)

--- src/main/kotlin/no/nb/mlt/wls/HermesApplication.kt
+++ src/main/kotlin/no/nb/mlt/wls/HermesApplication.kt
@@ -1,17 +1,13 @@
 package no.nb.mlt.wls
 
-import no.nb.mlt.wls.infrastructure.ProxyConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.context.ApplicationContext
-import kotlin.jvm.java
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
 class HermesApplication
 
 fun main(args: Array<String>) {
-    val context: ApplicationContext = runApplication<HermesApplication>(*args)
-    val proxyConfig = context.getBean(ProxyConfig::class.java)
+    runApplication<HermesApplication>(*args)
 }
