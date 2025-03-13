edcc215cc238f55ac3c6e96764d388db985a6d85
Daniel Aaron Salwerowicz <daniel@salwerowi.cz>
2025-03-13 15:11:27 +0100

    Rename Mongo service from `mongo1` to `mongo-db` in `compose.yaml`

--- docker/setup-replicaset.sh
+++ docker/setup-replicaset.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Set up replica set for the local database and run/load the init filen
-docker exec wls-local-mongo1-1 mongo -u root -p toor --eval "try {rs.conf()} catch(err) {rs.initiate({_id: 'rs0', members: [{_id: 0, host: 'mongo1:27017'}]})}"
-docker exec wls-local-mongo1-1 mongo -u root -p toor --eval "load('/mongo-init.js')"
+docker exec wls-local-mongo-db-1 mongo -u root -p toor --eval "try {rs.conf()} catch(err) {rs.initiate({_id: 'rs0', members: [{_id: 0, host: 'mongo1:27017'}]})}"
+docker exec wls-local-mongo-db-1 mongo -u root -p toor --eval "load('/mongo-init.js')"
