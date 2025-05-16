5eb477adb6354366abf5a377b1d6eb663a86ee0a
Tom Olsen <tom.olsen@nb.no>
2025-05-16 12:34:31 +0200

    MLT-149: Add endpoint and cronjob for processing outbox instead of scheduled task (#101)
    * Add endpoint and cronjob for processing outbox instead of scheduled task
    
    * Spotless fix
    
    * Extract script to own file and use configmaps to mount it in cronjob

---
+++ src/main/kotlin/no/nb/mlt/wls/application/adminapi/AdminController.kt
@@ -0,0 +1,33 @@
+package no.nb.mlt.wls.application.adminapi
+
+import io.swagger.v3.oas.annotations.tags.Tag
+import no.nb.mlt.wls.domain.model.events.catalog.CatalogEvent
+import no.nb.mlt.wls.domain.model.events.storage.StorageEvent
+import no.nb.mlt.wls.domain.ports.outbound.EventProcessor
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/hermes-admin/v1")
+@Tag(name = "Administration endpoints", description = """API for administrative tasks""")
+class AdminController(
+    private val catalogEventProcessor: EventProcessor<CatalogEvent>,
+    private val storageEventProcessor: EventProcessor<StorageEvent>
+) {
+    @PostMapping("/process-all-outboxes")
+    suspend fun processOutboxEvents() {
+        listOf(catalogEventProcessor, storageEventProcessor)
+            .forEach { it.processOutbox() }
+    }
+
+    @PostMapping("/process-catalog-outbox")
+    suspend fun processCatalogEvents() {
+        catalogEventProcessor.processOutbox()
+    }
+
+    @PostMapping("/process-storage-outbox")
+    suspend fun processStorageEvents() {
+        storageEventProcessor.processOutbox()
+    }
+}
