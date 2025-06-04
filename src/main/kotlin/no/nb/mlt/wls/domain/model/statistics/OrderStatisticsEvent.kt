7df6e1dd40720ff12e32dc712997eb24d8e23631
Daniel Aaron Salwerowicz <Daniel@Salwerowi.cz>
2025-06-04 10:52:42 +0200

    Fix how the timestamp in statistics messages is serialized (#119)
    We found that the old timestamp format was not working well for us in ES, so for clarity we switched them to use ISO date instead

--- src/main/kotlin/no/nb/mlt/wls/domain/model/statistics/OrderStatisticsEvent.kt
+++ src/main/kotlin/no/nb/mlt/wls/domain/model/statistics/OrderStatisticsEvent.kt
@@ -1,11 +1,14 @@
 package no.nb.mlt.wls.domain.model.statistics
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer
 import java.time.Instant
 
 data class OrderStatisticsEvent(
     val orderId: String,
     override val eventType: String,
     override val details: Map<String, Any>,
+    @JsonSerialize(using = ToStringSerializer::class)
     override val timestamp: Instant = Instant.now()
 ) : StatisticsEvent {
     override val id: String
