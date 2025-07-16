6eee0ff8e3537d2f7b6eb0ff63a0e1113087c4fb
Noah Bjerkli Aanonli <noah.aanonli@nb.no>
2025-07-16 08:26:39 +0200

    MLT-0147 Improve Interface and function names (#138)
    * Rename itemsPickedMap -> pickedItems
    
    * Rename pickedHostIds -> pickedItemIds
    
    * Rename OrderStatusUpdate -> UpdateOrderStatus

--- src/main/kotlin/no/nb/mlt/wls/domain/ports/inbound/UpdateOrderStatus.kt
+++ src/main/kotlin/no/nb/mlt/wls/domain/ports/inbound/UpdateOrderStatus.kt
@@ -3,7 +3,7 @@ package no.nb.mlt.wls.domain.ports.inbound
 import no.nb.mlt.wls.domain.model.HostName
 import no.nb.mlt.wls.domain.model.Order
 
-fun interface OrderStatusUpdate {
+fun interface UpdateOrderStatus {
     @Throws(OrderNotFoundException::class)
     suspend fun updateOrderStatus(
         hostName: HostName,
