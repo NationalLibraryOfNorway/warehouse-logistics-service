6d2659f1070a466053cadacecc0096abb4ec1049
Daniel Aaron Salwerowicz <daniel@salwerowi.cz>
2025-08-07 14:03:59 +0200

    Finish model documentation

--- src/main/kotlin/no/nb/mlt/wls/domain/model/Environment.kt
+++ src/main/kotlin/no/nb/mlt/wls/domain/model/Environment.kt
@@ -5,16 +5,7 @@ package no.nb.mlt.wls.domain.model
  * It's not a guarantee that the object will be stored there.
  */
 enum class Environment {
-    /**
-     * The object should be stored in a freezer.
-     */
     FREEZE,
-    /**
-     * The object should be stored in an environment suited for fragile materials.
-     */
     FRAGILE,
-    /**
-     * No preference for the storage environment.
-     */
     NONE
 }
--- src/main/kotlin/no/nb/mlt/wls/domain/model/Item.kt
+++ src/main/kotlin/no/nb/mlt/wls/domain/model/Item.kt
@@ -5,9 +5,33 @@ import no.nb.mlt.wls.domain.ports.inbound.ValidationException
 
 private val logger = KotlinLogging.logger {}
 
+/**
+ * Marks the location of an item that is with a lender.
+ * Ensures we use the same value across the application to avoid confusion.
+ */
 const val WITH_LENDER_LOCATION = "WITH_LENDER"
+
+/**
+ * Marks the location of an item when we don't know where it is.
+ * Used when the item is just created and we don't have a location for it yet.
+ * Ensures we use the same value across the application to avoid confusion.
+ */
 const val UNKNOWN_LOCATION = "UNKNOWN"
 
+/**
+ * Represents an item in the storage system.
+ * It contains information about the item that is relevant for the storage operations, it should not contain any catalog-specific information.
+ *
+ * @property hostId The unique identifier for the item in the host system.
+ * @property hostName The name of the host system which owns the item.
+ * @property description A brief description of the item.
+ * @property itemCategory The category of the item.
+ * @property preferredEnvironment The preferred storage environment for the item.
+ * @property packaging The packaging type of the item.
+ * @property callbackUrl An optional URL for callbacks related to the item.
+ * @property location The current location of the item.
+ * @property quantity The quantity of the item available in stock.
+ */
 class Item(
     val hostId: String,
     val hostName: HostName,
@@ -31,11 +55,20 @@ class Item(
     var quantity: Int = quantity
         private set
 
+    /**
+     * Picks a specified amount of items from the stock.
+     * If the amount picked exceeds the available quantity, it logs an error and sets the quantity
+     * to zero, indicating that the item is no longer available.
+     * If after picking the quantity becomes zero, it sets the location to `WITH_LENDER_LOCATION`.
+     *
+     * @param amountPicked The number of items to pick from the stock.
+     * @return The updated Item instance with the new quantity.
+     */
     fun pick(amountPicked: Int): Item {
         val itemsInStockQuantity = quantity
 
-        // In the case of over-picking, log it and set quantity to zero.
-        // This is done in hope that on return the database recovers
+        // In the case of over-picking, log it and set the quantity to zero.
+        // This is done in the hope that on return the database recovers
         if (amountPicked > itemsInStockQuantity) {
             logger.error {
                 "Tried to pick too many items for item with id '$hostId'. " +
@@ -51,6 +84,16 @@ class Item(
         return this
     }
 
+    /**
+     * Synchronizes the quantity and location of the item.
+     * Use it for stock sync updates from storage systems.
+     * If the location is null and quantity is not zero, it throws a ValidationException.
+     *
+     * @param quantity The new quantity to set for the item.
+     * @param location The new location to set for the item.
+     * @throws ValidationException if the location is null and quantity is not zero.
+     */
+    @Throws(ValidationException::class)
     fun synchronizeQuantityAndLocation(
         quantity: Int,
         location: String?
