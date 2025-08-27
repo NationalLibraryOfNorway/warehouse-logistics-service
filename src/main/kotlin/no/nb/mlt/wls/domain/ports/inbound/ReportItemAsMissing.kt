b7098caff2f2b29ac0ba9124f950acc0a3127b5a
Noah Bjerkli Aanonli <noah.aanonli@nb.no>
2025-08-27 09:20:04 +0200

    Fix missing imports

--- src/main/kotlin/no/nb/mlt/wls/domain/ports/inbound/ReportItemAsMissing.kt
+++ src/main/kotlin/no/nb/mlt/wls/domain/ports/inbound/ReportItemAsMissing.kt
@@ -2,14 +2,16 @@ package no.nb.mlt.wls.domain.ports.inbound
 
 import no.nb.mlt.wls.domain.model.HostName
 import no.nb.mlt.wls.domain.model.Item
+import no.nb.mlt.wls.domain.model.MISSING
 import no.nb.mlt.wls.domain.model.Order
+import no.nb.mlt.wls.domain.ports.inbound.exceptions.ItemNotFoundException
 
 /**
  * A port for marking items as missing.
  *
  * This interface is designed to handle cases where a storage handler reports an item as missing.
  * The implementation should update the item identified by its unique host name and host ID,
- * and then update its location to [no.nb.mlt.wls.domain.model.MISSING].
+ * and then update its location to [MISSING].
  *
  * This should also update any related orders which contain the item, so that they can be completed.
  *
