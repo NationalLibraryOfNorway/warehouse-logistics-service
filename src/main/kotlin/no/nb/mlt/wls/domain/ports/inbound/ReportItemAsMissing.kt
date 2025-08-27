983e9c94ee7c002ba558c89e79f0c717dbce9228
Noah Bjerkli Aanonli <noah.aanonli@nb.no>
2025-08-27 09:05:16 +0200

    Add documentation on ReportItemAsMissing interface

--- src/main/kotlin/no/nb/mlt/wls/domain/ports/inbound/ReportItemAsMissing.kt
+++ src/main/kotlin/no/nb/mlt/wls/domain/ports/inbound/ReportItemAsMissing.kt
@@ -2,8 +2,24 @@ package no.nb.mlt.wls.domain.ports.inbound
 
 import no.nb.mlt.wls.domain.model.HostName
 import no.nb.mlt.wls.domain.model.Item
+import no.nb.mlt.wls.domain.model.Order
 
+/**
+ * A port for marking items as missing.
+ *
+ * This interface is designed to handle cases where a storage handler reports an item as missing.
+ * The implementation should update the item identified by its unique host name and host ID,
+ * and then update its location to [no.nb.mlt.wls.domain.model.MISSING].
+ *
+ * This should also update any related orders which contain the item, so that they can be completed.
+ *
+ * This operation may throw an [ItemNotFoundException] if the item with the specified host ID cannot be found.
+ * @see Item
+ * @see Order.markMissing
+ * @see ItemNotFoundException
+ */
 fun interface ReportItemAsMissing {
+    @Throws(ItemNotFoundException::class)
     suspend fun reportItemMissing(
         hostName: HostName,
         hostId: String
