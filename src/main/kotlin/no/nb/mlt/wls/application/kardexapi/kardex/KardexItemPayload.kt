3543eb80d49434909107fb6116854316f879bc26
Daniel Aaron Salwerowicz <daniel@salwerowi.cz>
2025-09-30 12:38:17 +0200

    Hotfix for the kardex models validation

--- src/main/kotlin/no/nb/mlt/wls/application/kardexapi/kardex/KardexItemPayload.kt
+++ src/main/kotlin/no/nb/mlt/wls/application/kardexapi/kardex/KardexItemPayload.kt
@@ -3,6 +3,7 @@ package no.nb.mlt.wls.application.kardexapi.kardex
 import io.github.oshai.kotlinlogging.KotlinLogging
 import no.nb.mlt.wls.domain.model.HostName
 import no.nb.mlt.wls.domain.ports.inbound.UpdateItem
+import no.nb.mlt.wls.domain.ports.inbound.exceptions.ValidationException
 
 private val logger = KotlinLogging.logger {}
 
@@ -11,21 +12,29 @@ interface KardexItemPayload {
     val hostId: String
     val quantity: Double
     val location: String
-}
+    val operator: String
+    val motiveType: MotiveType
 
-fun KardexItemPayload.toUpdateItemPayload(): UpdateItem.UpdateItemPayload {
-    val resolvedHostName =
-        try {
-            HostName.fromString(hostName)
-        } catch (e: IllegalArgumentException) {
-            logger.warn { "Invalid hostname '$hostName' provided, defaulting to UNKNOWN. Error: ${e.message}" }
-            HostName.UNKNOWN
+    fun toUpdateItemPayload(): UpdateItem.UpdateItemPayload {
+        val resolvedHostName =
+            try {
+                HostName.fromString(hostName)
+            } catch (e: IllegalArgumentException) {
+                logger.warn { "Invalid hostname '$hostName' provided, defaulting to UNKNOWN. Error: ${e.message}" }
+                HostName.UNKNOWN
+            }
+
+        return UpdateItem.UpdateItemPayload(
+            hostName = resolvedHostName,
+            hostId = hostId,
+            quantity = quantity.toInt(),
+            location = location
+        )
+    }
+
+    fun validate() {
+        if (motiveType !in listOf(MotiveType.Deleted) && location.isBlank()) {
+            throw ValidationException("Location can not be blank for a regular payload")
         }
-
-    return UpdateItem.UpdateItemPayload(
-        hostName = resolvedHostName,
-        hostId = hostId,
-        quantity = quantity.toInt(),
-        location = location
-    )
+    }
 }
--- src/main/kotlin/no/nb/mlt/wls/application/kardexapi/kardex/KardexMaterialPayload.kt
+++ src/main/kotlin/no/nb/mlt/wls/application/kardexapi/kardex/KardexMaterialPayload.kt
@@ -3,7 +3,6 @@ package no.nb.mlt.wls.application.kardexapi.kardex
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.PositiveOrZero
-import no.nb.mlt.wls.domain.ports.inbound.exceptions.ValidationException
 
 @Schema(
     description = "Payload with updates status for material in Kardex.",
@@ -19,15 +18,15 @@ import no.nb.mlt.wls.domain.ports.inbound.exceptions.ValidationException
 )
 data class KardexMaterialPayload(
     @field:Schema(
-        description = """The main material ID of the item."""
-    )
-    @field:NotBlank(message = "Item ID can not be blank")
-    override val hostId: String,
-    @field:Schema(
         description = """Name of the host system which the material belongs to.""",
         example = "AXIELL"
     )
     override val hostName: String,
+    @field:Schema(
+        description = """The main material ID of the item."""
+    )
+    @field:NotBlank(message = "Item ID can not be blank")
+    override val hostId: String,
     @field:Schema(
         description = """The current quantity of the item."""
     )
@@ -36,17 +35,13 @@ data class KardexMaterialPayload(
     @field:Schema(
         description = """Name of the warehouse where the order materials/items are located."""
     )
-    @field:NotBlank(message = "Location can not be blank")
     override val location: String,
     @field:Schema(
         description = """The name of the person who updated/operated on the Kardex system."""
     )
-    val operator: String,
-    val motiveType: MotiveType
-) : KardexItemPayload {
-    fun validate() {
-        if (motiveType !in listOf(MotiveType.Deleted) && location.isBlank()) {
-            throw ValidationException("Location can not be blank for a regular payload")
-        }
-    }
-}
+    override val operator: String,
+    @field:Schema(
+        description = """Describes the motive for the update message, such as deletion."""
+    )
+    override val motiveType: MotiveType
+) : KardexItemPayload
