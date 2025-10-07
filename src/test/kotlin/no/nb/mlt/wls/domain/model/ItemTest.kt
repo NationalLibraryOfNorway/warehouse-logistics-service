444697515ca9c14c72974c8367fb5ec3a4b5ca00
Noah Bjerkli Aanonli <noah.aanonli@nb.no>
2025-10-07 13:00:08 +0200

    Add unit test for the same case

---
+++ src/test/kotlin/no/nb/mlt/wls/domain/model/ItemTest.kt
@@ -0,0 +1,15 @@
+package no.nb.mlt.wls.domain.model
+
+import no.nb.mlt.wls.createTestItem
+import org.junit.jupiter.api.Test
+
+class ItemTest {
+    @Test
+    fun `synchronizeItem should only update item when item exists within another storage system`() {
+        val expectedItem = createTestItem(quantity = 1, location = "SOMEWHERE_IN_KARDEX", associatedStorage = AssociatedStorage.KARDEX)
+        val testItem = createTestItem(quantity = 1, location = "SOMEWHERE_IN_KARDEX", associatedStorage = AssociatedStorage.KARDEX)
+        testItem.synchronizeItem(0, "MISSING", AssociatedStorage.SYNQ)
+        assert(testItem.associatedStorage != AssociatedStorage.SYNQ)
+        assert(testItem == expectedItem)
+    }
+}
