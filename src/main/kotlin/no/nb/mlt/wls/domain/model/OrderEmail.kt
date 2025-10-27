a789f0f6c8b0dc517679893265b2cf3f4398c0b0
Noah Bjerkli Aanonli <noah.aanonli@nb.no>
2025-10-27 10:00:52 +0100

    Add minimum data for storing an email

---
+++ src/main/kotlin/no/nb/mlt/wls/domain/model/OrderEmail.kt
@@ -0,0 +1,17 @@
+package no.nb.mlt.wls.domain.model
+
+data class OrderEmail(
+    val hostOrderId: String,
+    val hostName: HostName,
+    val orderType: Order.Type,
+    val contactPerson: String,
+    val contactEmail: String,
+    val note: String,
+    val orderLines: List<OrderLine>
+) {
+    data class OrderLine(
+        val hostId: String,
+        val description: String,
+        val location: String
+    )
+}
