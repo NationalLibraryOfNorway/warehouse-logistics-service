701b67b4577c911c599f90d89e5aa9b858e4a53c
Noah Bjerkli Aanonli <noah.aanonli@nb.no>
2025-11-21 15:31:56 +0100

    Fix bad email event documentation

--- src/main/kotlin/no/nb/mlt/wls/domain/model/events/email/EmailEvent.kt
+++ src/main/kotlin/no/nb/mlt/wls/domain/model/events/email/EmailEvent.kt
@@ -5,7 +5,7 @@ import no.nb.mlt.wls.domain.model.events.Event
 /**
  * Represents an email-related event in the system. Email events are created in reaction to
  * items or orders in the system. For example when an order is created, it will emit an
- * [OrderConfirmationMail] event and an [OrderHandlerMail] event.
+ * [OrderConfirmationMail] event and an [OrderPickupMail] event.
  *
  * This sealed interface ensures that all implementations are known at compile-time, allowing for
  * exhaustive handling of all event types by the processor.
@@ -17,6 +17,5 @@ import no.nb.mlt.wls.domain.model.events.Event
  * The email events also contain the following properties:
  * - An unique identifier inherited from the [Event] interface
  * - A body representing the contents of the email (e.g. an order, or an order with its [no.nb.mlt.wls.domain.model.Item]s)
- * - A timestamp denoting when the event occurred.
  */
 sealed interface EmailEvent : Event
