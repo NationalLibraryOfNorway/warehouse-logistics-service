011490be13f3efb272e5581ab76ea9de1091df7c
Noah Bjerkli Aanonli <noah.aanonli@nb.no>
2025-12-02 14:00:52 +0100

    Add Object Mapper config

---
+++ src/main/kotlin/no/nb/mlt/wls/infrastructure/config/ObjectMapperConfig.kt
@@ -0,0 +1,14 @@
+package no.nb.mlt.wls.infrastructure.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ObjectMapperConfig {
+
+    @Bean
+    fun objectMapper(): ObjectMapper {
+       return ObjectMapper()
+    }
+}
