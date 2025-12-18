85a56b9493c0f17fc3cc7c522727d8a836305069
Daniel Aaron Salwerowicz <daniel@salwerowi.cz>
2025-12-18 12:41:23 +0100

    Make logic more robust

--- src/main/kotlin/no/nb/mlt/wls/infrastructure/callbacks/InventoryNotifierAdapter.kt
+++ src/main/kotlin/no/nb/mlt/wls/infrastructure/callbacks/InventoryNotifierAdapter.kt
@@ -107,11 +107,10 @@ class InventoryNotifierAdapter(
         if (error is WebClientResponseException) {
             // 4xx errors - client errors that should not be retried
             if (error.statusCode.is4xxClientError) {
-                if (!callbackUrl.contains("asta")) {
-                    logger.error(error) {
-                        "Received 4xx error (${error.statusCode.value()}) from callback URL: $callbackUrl, " +
+                if (callbackUrl.contains("asta") && error.statusCode.value() == 400) return true
+                logger.error(error) {
+                    "Received 4xx error (${error.statusCode.value()}) from callback URL: $callbackUrl, " +
                             "we will never retry sending this message: $payload"
-                    }
                 }
                 return true
             }
