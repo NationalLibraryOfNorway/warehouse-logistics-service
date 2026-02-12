7ae23435dcc8e4a5693c37e4c8fb3e309d0cb9ef
Daniel Aaron Salwerowicz <daniel@salwerowi.cz>
2026-02-12 14:55:07 +0100

    add support for dynamic server URLs in Swagger configuration

--- src/main/kotlin/no/nb/mlt/wls/application/adminapi/config/SwaggerConfig.kt
+++ src/main/kotlin/no/nb/mlt/wls/application/adminapi/config/SwaggerConfig.kt
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
 import io.swagger.v3.oas.annotations.security.OAuthFlow
 import io.swagger.v3.oas.annotations.security.OAuthFlows
 import io.swagger.v3.oas.annotations.security.SecurityScheme
+import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.servers.Server
 import org.springdoc.core.models.GroupedOpenApi
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -22,15 +25,31 @@ import org.springframework.context.annotation.Configuration
                 )
         )
 )
-class SwaggerConfig {
+class SwaggerConfig(
+    @param:Value($$"${springdoc.server-url}") private val serverUrl: String
+) {
     @Bean("adminApi")
     fun adminApi(): GroupedOpenApi =
         GroupedOpenApi
             .builder()
             .group("Admin API")
             .displayName("Admin API")
-            .pathsToMatch("/hermes/admin/**")
             .addOpenApiCustomizer {
+                it.info.title = "Kardex updates port for Hermes WLS"
+                it.info.description =
+                    """
+                    Hermes, developed and maintained by the Warehouse and Logistics team (MLT) at the National Library of Norway (NLN).
+                    Hermes facilitates communication between the NLN's storage systems and catalogues, serving as a master system.
+
+                    This submodule of Hermes provides system administrators an easy way to trigger admin specific jobs when needed.
+                    """.trimIndent()
+                it.info.contact =
+                    Contact()
+                        .name("MLT team at the National Library of Norway")
+                        .email("mlt@nb.no")
+                        .url("https://www.nb.no")
                 it.security = listOf((SecurityRequirement().addList("clientCredentials")))
-            }.build()
+                it.servers = listOf(Server().url(serverUrl))
+            }.pathsToMatch("/hermes/admin/**")
+            .build()
 }
