19671def9d11acde02d2e6d2ac0c7aa3e472a74b
Noah Bjerkli Aanonli <noah.aanonli@nb.no>
2026-02-16 13:06:55 +0100

    Use ConditionalOnBooleanProperty instead of a regular one
    Might be causing some issues when deployed

--- src/main/kotlin/no/nb/mlt/wls/infrastructure/kardex/KardexAdapter.kt
+++ src/main/kotlin/no/nb/mlt/wls/infrastructure/kardex/KardexAdapter.kt
@@ -12,7 +12,7 @@ import no.nb.mlt.wls.domain.ports.outbound.exceptions.StorageSystemException
 import no.nb.mlt.wls.infrastructure.config.TimeoutProperties
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.toEntity
@@ -22,7 +22,7 @@ import java.util.concurrent.TimeoutException
 private val logger = KotlinLogging.logger {}
 
 @Component
-@ConditionalOnProperty(value = ["kardex.enabled"], havingValue = "true")
+@ConditionalOnBooleanProperty(value = ["kardex.enabled"], havingValue = true)
 class KardexAdapter(
     @param:Qualifier("nonProxyWebClient")
     private val webClient: WebClient,
